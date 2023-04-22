4215f034c53baa4bf7fad27052bf5d40905b5d91
billy5982 <kuu0404@naver.com>
2023-04-22 21:05:08 +0900

    ⚙️ setting : App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import GlobalStyles from './styles/reset';
+import { lightTheme, defalutTheme } from './styles/theme';
+import { modeState } from './states';
+import Layout from './pages/Layout';
+import Routes from './routes';
 
 function App() {
   const currentMode = useRecoilValue(modeState);
