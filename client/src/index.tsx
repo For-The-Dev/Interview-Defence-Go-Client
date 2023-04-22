c79540a4bff15393fe729287a89c3c1d1e3a16ed
billy5982 <kuu0404@naver.com>
2023-04-22 21:10:51 +0900

    🐛  fix : import 삭제

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { ThemeProvider } from 'styled-components';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
