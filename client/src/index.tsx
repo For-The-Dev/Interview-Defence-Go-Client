0e35c655f735656de0657d514436a2588ae27173
billy5982 <kuu0404@naver.com>
2023-05-04 01:36:39 +0900

    ✨ feat : React-query Setting 관련 분리 완료

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from './react-query';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-const queryClient = new QueryClient();
 
 root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <App />
-        {/* ReactQuery를 디버깅할 수 있게 해줌 */}
-        <ReactQueryDevtools />
-      </RecoilRoot>
-    </QueryClientProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <App />
+      {/* ReactQuery를 디버깅할 수 있게 해줌 */}
+      <ReactQueryDevtools />
+    </RecoilRoot>
+  </QueryClientProvider>,
+  // </React.StrictMode>,
 );
---
+++ client/src/react-query/queryKey.ts
@@ -0,0 +1,5 @@
+const queryKey = {
+  userInfo: 'userInfo',
+  userQA: 'answerPreview',
+};
+export default queryKey;
