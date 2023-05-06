c8060555a1177227f8f7a8f6e1879c433f4c792d
billy5982 <kuu0404@naver.com>
2023-05-06 21:55:34 +0900

    🐛 fix : 쿼리 키 수정

--- client/src/react-query/queryKey.ts
+++ client/src/react-query/queryKey.ts
@@ -1,7 +1,7 @@
 const queryKey = {
   // 나중에 답변 제출 후 세가지를 모두 invalidateQueries로 무효화 시킬 것.
   userInfo: 'userInfo',
-  userQA: 'answerPreview',
+  userQAPreview: 'answerPreview',
   detailQA: 'detailQA',
   getInterviews: 'getInterviews',
   answer: 'answer',
