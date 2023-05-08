f668f421d53fa34bbcfc855a1b40f8e7a36d2411
billy5982 <kuu0404@naver.com>
2023-05-08 15:51:27 +0900

    🐛 fix :proxy 제거

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios';
 
-const baseUrl = `${process.env.REACT_APP_SERVER_URL}/api`;
+const baseUrl = process.env.REACT_APP_SERVER_URL;
 axios.defaults.withCredentials = true;
 
 const Api = axios.create({
