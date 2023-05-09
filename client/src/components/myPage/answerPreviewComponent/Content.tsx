22f4be1061b03a0762448a86f2921e524c230428
billy5982 <kuu0404@naver.com>
2023-05-09 13:53:41 +0900

    🐛 fix : UTC 시간 기준의 날짜 데이터 수정

--- client/src/components/myPage/answerPreviewComponent/Content.tsx
+++ client/src/components/myPage/answerPreviewComponent/Content.tsx
@@ -11,8 +11,8 @@ interface ContentProps {
 }
 
 const Content = ({ createdAt, question, answer, id, changeModalState }: ContentProps) => {
-  console.log(dayjs(createdAt).add(9, 'hour'));
-  const editQuestionDate = dayjs(createdAt).add(9, 'hour').format('YYYY-MM-DD');
+  console.log(dayjs(createdAt).subtract(9, 'hour'));
+  const editQuestionDate = dayjs(createdAt).subtract(9, 'hour').format('YYYY-MM-DD');
 
   return (
     <S.QuestionWrapper>
