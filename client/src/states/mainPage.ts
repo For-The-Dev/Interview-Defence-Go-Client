cbd88c3f6b4fedce04cb5cfa946bbea975897988
billy5982 <kuu0404@naver.com>
2023-06-06 00:54:52 +0900

    ✨ feat : 버튼 리렌더링 방지 관련 selector 추가

--- client/src/states/mainPage.ts
+++ client/src/states/mainPage.ts
@@ -1,8 +1,16 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const stackState = atom<string[]>({
   key: 'stack',
   default: [],
+});
+
+export const checkSelectStack = selector<boolean>({
+  key: 'stacksSet',
+  get: ({ get }) => {
+    const stack = get(stackState);
+    return stack.length > 0 ? true : false;
+  },
 });
 
 export const modalState = atom<boolean>({
